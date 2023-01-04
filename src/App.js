11b4d1a82118a80813b5bb48fd668a56f31998fd
youssefrhanem <youssefrhanem91@gmail.com>
2023-01-04 23:06:33 +0100

    creaate pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <h1 className="text-2xl font-bold underline bg-red-50">
-      Hello world!
-    </h1>
+  <>
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      <Route path='/offers' element={<Offers/>}/>
+    </Routes>
+    
+  </Router>
+  </>
   );
 }
 
